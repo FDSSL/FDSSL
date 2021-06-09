2ec059c5a353201a4ed7d1bd364cea9b9eaf38b0
Benjamin Friedman <friedman.benjamin@gmail.com>
2021-06-09 15:38:59 -0700

    fix parser

--- src/Main.hs
+++ src/Main.hs
@@ -9,12 +9,33 @@ import Parser
 main :: IO ()
 main = putStrLn $ prettySafe prog1
 
+-- | Evaluates an FDSSL program, and prints the results as GLSL
 evalProg :: String -> IO ()
 evalProg fn = do
   r <- parseFDSSLFile fn
   case r of
     (Left l)  -> putStrLn $ show l
     (Right progList) -> mapM_ putStrLn $ map (pretty . snd) progList
+
+-- | Crude representation for now
+-- TODO Change this for a proper representation style later...
+type GLSLProg = String
+
+-- | Compiles an FDSSL program into a GLSL Program
+--compileProgram :: Prog -> GLSLProg
+--compileProgram p =
+
+-- | Interprets & compiles FDSSL to GLSL, and writes to a file w/ the same name
+{-
+compile :: String -> IO ()
+compile fn = do
+  r <- parseFDSSLFile fn
+  case r of
+    (Left l)  -> putStrLn $ show l -- failed...
+    (Right progList) -> mapM_ compileProgram progList
+-}
+
+      --mapM_ putStrLn $ map (pretty . snd) progList
 
 -- needs fixing or removal
 -- main2 :: IO ()
